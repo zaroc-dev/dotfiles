64c63c8344a84abee299e9ef537d88384290e817
zaroc <arthuraktamirov@gmail.com>
2026-09-23 00:04:53 +0200

    feat(input): enable Wooting device support

--- modules/core/input.nix
+++ modules/core/input.nix
@@ -1,5 +1,7 @@
 { ... }: {
   flake.nixosModules.input = { ... }: {
+    # Install Wootility and grant device access for the Wooting 80HE.
+    hardware.wooting.enable = true;
     hardware.opentabletdriver.enable = true;
     hardware.uinput.enable = true;
 
